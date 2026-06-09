bddfd29c4299e0b24289455d27af0877fc2bbc3e
Rakshit Yadav <yadavrakshit60@gmail.com>
2026-06-09 17:06:49 +0530

    implemented queing
    Signed-off-by: Rakshit Yadav <yadavrakshit60@gmail.com>

---
+++ autoworkers/internal/queue/queue.go
@@ -0,0 +1,31 @@
+package queue
+
+import "autoworkers/internal/job"
+
+type Queue struct{
+	JobId chan string
+}
+
+func Constructor() *Queue{
+	channel := make(chan string,100)
+	p := &Queue{
+		JobId: channel,
+	}
+	return p
+
+}
+
+func Enqueue(p *Queue, x *job.Job){
+	p.JobId <- x.ID
+
+}
+
+func Dequeue(p *Queue) string{
+	value := <- p.JobId
+	return value
+	
+}
+
+func Close(p *Queue){
+	close(p.JobId)
+}
