2c05b175094d7ee2d35edf209326074f907b4cc2
Rakshit Yadav <yadavrakshit60@gmail.com>
2026-06-11 00:43:53 +0530

    testing main.go
    Signed-off-by: Rakshit Yadav <yadavrakshit60@gmail.com>

--- autoworkers/cmd/server/main.go
+++ autoworkers/cmd/server/main.go
@@ -1,9 +1,30 @@
 package main
 
 import (
+	"autoworkers/internal/job"
+	"autoworkers/internal/queue"
+	"autoworkers/internal/store"
+	"autoworkers/internal/worker"
 	"fmt"
+	"time"
 )
 
 func main(){
-	fmt.Println("server")
+	s := store.Constructor()
+	q := queue.Constructor()
+	w :=worker.Constructor(q, s)
+	go worker.Workers(w)
+	testjob := &job.Job{
+	ID: "job-1",
+	Type: "test",
+	Payload: "hello world",
+	Status: job.Pending,
+	}
+	store.Create(testjob,s)
+	queue.Enqueue(q,testjob)
+	
+	time.Sleep(2* time.Second)
+	fmt.Println(testjob.Status)
+	fmt.Println(testjob.Result)
+
 }
